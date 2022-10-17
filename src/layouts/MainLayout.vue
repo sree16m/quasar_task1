f7f881b1ddef41af9fee745fcb4ca7db716b1aa7
Srikanth <sree16m@gmail.com>
2022-10-17 23:10:54 +0530

    one more tyr

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,81 @@
+<template>
+  <q-layout view="lHh lpR fFf">
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered class="text-white" :width="340">
+      <!-- drawer content -->
+      <q-scroll-area class="fit q-pa-lg q-pt-lg">
+        <!-- div class="q-pa-lg q-pt-lg" -->
+          <q-toolbar>
+            <img src="~assets/Logo.svg">
+          </q-toolbar>
+          <q-toolbar class="q-mb-xl">
+            <span class="caption text-subtitle2">
+              Trusted way of banking for 3,000+ SMEs and startups in Singapore
+            </span>
+          </q-toolbar>
+          <!--q-list-->
+              <q-item v-for="link in links" :key="link.text" clickable class="q-mt-xl q-mb-xl">
+                <q-item-section avatar>
+                  <q-icon color="grey" :name="link.icon" />
+                </q-item-section>
+                <q-item-section>
+                  <q-item-label>{{ link.text }}</q-item-label>
+                </q-item-section>
+              </q-item>
+          <!--/q-list-->
+        <!--/div -->
+      </q-scroll-area>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer elevated class="bg-white lt-md">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-tabs  v-model="tab" class="bg-white text-grey-4">
+            <q-tab v-for="link in links" :key="link.text" :icon="link.tabicon" :label="link.text"></q-tab>
+          </q-tabs>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+
+  </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  data () {
+    return {
+      tab: 'Home'
+    }
+  },
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      },
+      links: [
+        { icon: "img:icons/Home.svg", text: 'Home', tabicon: "img:icons/Logo1.svg"},
+        { icon: 'img:icons/Card.svg', text: 'Cards', tabicon: "img:icons/Card.svg"},
+        { icon: 'img:icons/Payments.svg', text: 'Payments', tabicon: "img:icons/Payments1.svg"},
+        { icon: 'img:icons/Credit.svg', text: 'Credit', tabicon: "img:icons/Credit1.svg"},
+        { icon: 'img:icons/Account.svg', text: 'Settings', tabicon: "img:icons/Account1.svg" }
+      ],
+    }
+  }
+}
+</script>
+<style lang="sass">
+.q-drawer
+  background: #0C365A !important
+
+.caption
+  opacity: 30%
+  font-size: 14px
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,50 @@
+<template>
+  <!--q-page class="flex flex-center q-page" -->
+  <q-page class="q-pt-lg q-pl-xl q-pr-xl">
+    <div class="row justify-between">
+        <div class="col-12">Available Balance</div>
+        <div>First column</div>
+        <div>Second column</div>
+      </div>
+    <!--q-toolbar >
+      <q-toolbar-title class="text-black">Available Balance </q-toolbar-title>
+
+      <q-btn flat round dense icon="whatshot" />
+    </q-toolbar-->
+    <q-tabs
+          v-model="tab"
+          dense
+          class="text-grey"
+          active-color="primary"
+          indicator-color="primary"
+          align="justify"
+          narrow-indicator
+    >
+      <q-tab name="mails" label="My debit cards" />
+      <q-tab name="alarms" label="All company cards" />
+    </q-tabs>
+    <q-card class="q-mt-sm">
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="mails">
+          <div class="text-h6">Mails</div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </q-tab-panel>
+        <q-tab-panel name="alarms">
+          <div class="text-h6">Alarms</div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </q-tab-panel>
+      </q-tab-panels>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      tab: 'mails'
+    }
+  }
+}
+</script>
